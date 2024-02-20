eca9a68d09e57e0c19a3c3ed6449c1503783f163
Sh0rtMort <mortonkman2024@gmail.com>
2024-02-19 22:01:07 -0600

    david discord told me so

--- src/main/java/frc/robot/commands/Shoot/AuxShoot.java
+++ src/main/java/frc/robot/commands/Shoot/AuxShoot.java
@@ -17,7 +17,8 @@ public class AuxShoot extends SequentialCommandGroup{
                 new PivotPIDCommandNonDegrees(shoot, Constants.ShootingConstants.pivotStore), // re-add this if we start using the pivot again
                 new IntakeSetpointCommand(intake, -3.6),
                 new InstantCommand(() -> intake.setPowerVolts(-2)),
-                new InstantCommand(() -> shoot.setIndexMotorVolts(8)),
+                // new InstantCommand(() -> shoot.setIndexMotorVolts(8)),
+                new InstantCommand(() -> shoot.setIndexMotorVolts(12)), //sped up because david said so
                 // new InstantCommand(() -> shoot.setIndexMotorVelocity(Constants.ShootingConstants.indexFeedVelocity)),
                 new RevShooter(shoot, Constants.ShootingConstants.targetShootingRPM)
             ).withTimeout(1.5),
